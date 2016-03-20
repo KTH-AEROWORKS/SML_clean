7b53c88cba6a2f77dd59d696a4daad7cd4ee9bcb
Antonio <antonio.adaldo.89@gmail.com>
2016-03-20 15:28:20 +0100

    Added some Trajectory classes This commit adds the implementation of a FixedPointTrajectory and a CircleTrajectory class. A simple ROS node is added to the scripts to test these classes.

---
+++ src/trajectories/fixed_point_trajectory.py
@@ -0,0 +1,33 @@
+"""This class implements a fixed-point trajectory,
+i.e., a trajectory that stays in the same point forever.
+"""
+
+import trajectory as tj
+import numpy as np
+
+
+class FixedPointTrajectory(tj.Trajectory):
+
+    @classmethod
+    def description(cls):
+        return "Fixed Point"
+        
+    def __str__(self):
+        string = self.description()
+        string += "\nPoint: " + str(self.get_offset())
+        return string
+        
+    def desired_trajectory(self, time):
+        pos = np.zeros(3)
+        vel = np.zeros(3)
+        acc = np.zeros(3)
+        jrk = np.zeros(3)
+        snp = np.zeros(3)
+        return pos, vel, acc, jrk, snp
+        
+        
+        
+        
+"""Test"""
+tr = FixedPointTrajectory()
+print tr
